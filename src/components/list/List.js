ea1cdbed04bdf7e454aede044c03757466cad5cc
chomin(green) <doaiso381@naver.com>
2024-05-09 16:28:41 +0900

    민지 수정

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,10 +1,15 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { DataContext } from "../../MainData";
-import { Card, Col, ListGroup, Row } from "react-bootstrap";
+import { Button, Card, Col, Form, ListGroup, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default function List() {
 
-    const { data } = useContext(DataContext);
+    const { data, setData } = useContext(DataContext);
+    const [list, setList] = useState(data);
+
+    // console.log(data)
+    // console.log(list);
 
     // 데이터가 비어 있는지 확인
     if (!data || data.length === 0) {
@@ -12,21 +17,22 @@ export default function List() {
     }
 
     return (
-        <Row xs={1} md={3} className="g-4">
-            {data.map((item, index) => (
-                <Col key={index}>
-                    <Card style={{ width: '18rem', height: '30rem'}}>
-                        <Card.Img variant="top" src={item.image} style={{ height: '15rem' }} />
-                        <Card.Body>
-                            <Card.Title style={{wordBreak: 'keep-all'}}>{item.title.replace(/[<b></b>]/g, '')}</Card.Title>
-                        </Card.Body>
-                        <ListGroup className="list-group-flush">
-                            <ListGroup.Item>{item.lprice}원</ListGroup.Item>
-                            <ListGroup.Item variant="success">장바구니</ListGroup.Item>
-                        </ListGroup>
-                    </Card>
-                </Col>
-            ))}
-        </Row>
+        <>
+            <Row xs={1} md={3} className="g-4">
+                {data.map((item, index) => (
+                    <Col key={index}>
+                        <Card style={{ width: '18rem', height: '30rem' }}>
+                            <Card.Img variant="top" src={item.image} style={{ height: '15rem' }} />
+                            <Card.Body>
+                                <Card.Title style={{ wordBreak: 'keep-all', height: '7rem'}}>{item.title.replace(/[<b></b>]/g, '')}</Card.Title>
+                                <Card.Title style={{padding: '5%'}}>{item.lprice}원</Card.Title>
+                                <Button variant="secondary">상세보기</Button>{' '}
+                                <Button variant="success">장바구니</Button>{' '}
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </>
     )
 }
