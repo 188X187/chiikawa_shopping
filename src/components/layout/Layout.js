377c99e4dc88a5a4a370cba01b286aa0fd19b4d9
User -2024LCOFV\Administrator <leegj420@gmail.com>
2024-05-09 15:23:22 +0900

    카트 추가

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -3,6 +3,7 @@ import Home from "../common/Home";
 import GlobalNav from "../common/GlobalNav";
 import { useContext } from "react";
 import { DataContext } from "../../MainData";
+import CartPage from "../cart/CartPage";
 
 function Layout(){    
     return(
@@ -12,7 +13,7 @@ function Layout(){
             <Route path="/" element={<Home/>}></Route>
             <Route path="list" element={<div>list page 설정하시구연</div>}></Route>
             <Route path="detail" element={<div>detail page 설정하시구연</div>}></Route>
-            <Route path="cart" element={<div>cart page 설정하시구연</div>}></Route>
+            <Route path="cart" element={<CartPage/>}></Route>
         </Routes>
         </>
     )
