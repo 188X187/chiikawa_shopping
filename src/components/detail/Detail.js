0a12a4451348c9053e71b7588967bea162b3ecc0
DESKTOP-014K42O\조민지 <doaiso381@naver.com>
2024-05-09 23:10:19 +0900

    민지-디테일 수정

--- src/components/detail/Detail.js
+++ src/components/detail/Detail.js
@@ -2,30 +2,28 @@ import { useContext, useEffect, useState } from "react";
 import Card from 'react-bootstrap/Card';
 import { DataContext } from "../../MainData";
 
-export default function Detail(props) {
+export default function Detail() {
     const { detail } = useContext(DataContext);
     const [infoData, setInfoData] = useState(null);
 
     useEffect(() => {
         
-        // `detail` 데이터가 변경되면 로컬스토리지에 저장하고 상태 업데이트
+        // `detail` 데이터가 변경되면 로컬스토리지에 저장, infoData에도 저장
         if (detail) {
             localStorage.setItem('DetailData', JSON.stringify(detail));
             setInfoData(detail);
         }
 
         // 컴포넌트가 처음 마운트될 때 실행
-        const storedData = localStorage.getItem('DetailData');
+        const localData = localStorage.getItem('DetailData');
         
-        // 로컬스토리지에 데이터가 있으면 상태를 업데이트
-        if (storedData) {
-            setInfoData(JSON.parse(storedData));
+        // 로컬스토리지에 데이터가 있으면 infoData에 그 데이터 저장
+        if (localData) {
+            setInfoData(JSON.parse(localData));
         }
-
-
     }, [detail]);
 
-    // infoData가 null인 경우 처리 (e.g. "No data" 메시지 표시)
+    // infoData가 null인 경우 처리 ("No data" 메시지 표시)
     if (!infoData) {
         return <p>No data</p>;
     }
