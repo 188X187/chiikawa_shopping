b5f5cdda76fdc62804f39d7b5ddc6e70fa3bcb24
SUH(green) <suh240505@gmail.com>
2024-05-09 17:33:18 +0900

    필터 생성중

---
+++ src/components/search/Filter.js
@@ -0,0 +1,15 @@
+export default function Filter(){
+    return(
+        <>
+        <div>
+            <h4>카테고리</h4>
+            <p>인형</p>
+            <p>피규어</p>
+            <p>가챠</p>
+            <p>카드</p>
+            <p>시계</p>
+            <p>마스코트</p>
+        </div>
+        </>
+    )
+}
