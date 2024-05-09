3917a49b4dc43c5da78bb19ba8b8ca65508b910f
User -2024LCOFV\Administrator <leegj420@gmail.com>
2024-05-09 18:03:30 +0900

    카트페이지, 카트리스트 분리

--- src/components/cart/CartList.js
+++ src/components/cart/CartList.js
@@ -1,4 +1,20 @@
 const CartList = () => {
+
+    const storageKeys = Object.keys(localStorage); 
+    const storageValues = storageKeys.map((key) => {
+        try {
+            return JSON.parse(localStorage.getItem(key)); 
+        } catch (error) {
+            return localStorage.getItem(key); 
+        }
+    });
+
+    return (
+        <>
+        {storageValues.map((item, index)=>(
+            <div key={index}>{item.title.replace(/[<b></b>]/g, '')}</div>))}
+        </>
+    )
     
 }
 
--- src/components/cart/CartPage.js
+++ src/components/cart/CartPage.js
@@ -1,27 +1,10 @@
-import { useContext } from "react";
-import { DataContext } from "../../MainData";
+import CartList from "./CartList";
 
 const CartPage = () => {
-
-    const storageKeys = Object.keys(localStorage); 
-    const storageValues = storageKeys.map((key) => {
-    try {
-        return JSON.parse(localStorage.getItem(key)); 
-    } catch (error) {
-        return localStorage.getItem(key); 
-    }
-});
     return (
-        <>
-            {
-            localStorage.length == 0 ?
-                <div>장바구니가 비었습니다</div>
-            :
-                <div></div>
-            }
-        </>
+        <CartList />
     )
-    
+
 }
 
 export default CartPage;
