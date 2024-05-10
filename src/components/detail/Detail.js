e79455bf4520b11d41dd14d3dcdb0034e2ae0bca
Sando0617(410) <kimhuuu@naver.com>
2024-05-10 16:57:11 +0900

    태호 상세페이지 css

--- src/components/detail/Detail.js
+++ src/components/detail/Detail.js
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import Card from 'react-bootstrap/Card';
 import { DataContext } from "../../MainData";
+import { Link } from "react-router-dom";
 
 export default function Detail() {
     const { detail } = useContext(DataContext);
@@ -21,14 +22,15 @@ export default function Detail() {
 
     return (
         <Card style={{ width: '45rem', height: '40rem', margin: 'auto' }}>
-            <Card.Title className='p-3'>{infoData && infoData.title.replace(/[<b></b>]/g, '')}</Card.Title>
-            <Card.Title className='p-3'style={{ wordBreak: 'keep-all' }}>{infoData.title.replace(/[<b></b>]/g, '')}</Card.Title>
+            <Card.Title className='p-3 border-bottom border-2 fw-bold'style={{ wordBreak: 'keep-all' }}>{infoData && infoData.title.replace(/[<b></b>]/g, '')}</Card.Title>
             <Card.Body>
                 <Card.Img style={{ width: '25rem' }} className='Infocardimg' variant="top" src={infoData && infoData.image} />
-                <Card.Title style={{ padding: '3%' }}>{infoData && infoData.lprice}원</Card.Title>
-                <div className='d-flex'>
+                <Card.Title className='fw-bold text-danger' style={{ padding: '2%' }}>{infoData && infoData.lprice}원</Card.Title>
+                <div className='d-flex justify-content-between'>
                     <button className='w-50 border-0 p-3 mb-2 bg-success text-white'>구매</button>
-                    <button className='w-50 border-0 p-3 mb-2 bg-secondary text-white'>장바구니</button>
+                    <Link to={'/cart'} className='w-50 border-0 p-3 mb-2 bg-secondary text-white'>
+                        <button className='border-0 bg-secondary text-white'>장바구니</button>
+                    </Link>
                 </div>
             </Card.Body>
             
