960632c99d3eb991d50023c97a12807720ccee25
chomin(green) <doaiso381@naver.com>
2024-05-14 13:14:05 +0900

    민지-카테고리 스타일 및 푸터 정렬 수정

--- src/components/home/Footer.js
+++ src/components/home/Footer.js
@@ -2,26 +2,26 @@ import { Col, Container, Row } from "react-bootstrap";
 
 export default function Footer() {
   return (
-    <footer className="bg-body-tertiary mb-3 footer">
+    <footer className="bg-body-tertiary p-3 footer">
+      <Row>
+        <Col>
+          <img src="http://fiximage.10x10.co.kr/web2021/cscenter/icon_sns_instagram.png" alt="insta" />
+          <img src="http://fiximage.10x10.co.kr/web2021/cscenter/icon_sns_facebook.png" alt="facebook" />
+          <img src="http://fiximage.10x10.co.kr/web2021/cscenter/icon_sns_you.png" alt="yotube" />
+        </Col>
+      </Row>
 
-        <ul>
-          <li>
-            <img src="http://fiximage.10x10.co.kr/web2021/cscenter/icon_sns_instagram.png" alt="insta" />
-          </li>
-          <li>
-            <img src="http://fiximage.10x10.co.kr/web2021/cscenter/icon_sns_facebook.png" alt="facebook" />
-          </li>
-          <li>
-            <img src="http://fiximage.10x10.co.kr/web2021/cscenter/icon_sns_you.png" alt="yotube" />
-          </li>
-        </ul>
+      <hr />
 
-        <hr />
+      <Row>
+        <Col>
+          <p>호스팅서비스:㈜농담곰</p>
+          <p>사이트 내 판매 상품 중 일부는 ㈜농담곰에 입점한 개별판매자가 판매하는 상품이 포함되어 있습니다. 해당 상품의 경우
+            ㈜농담곰은 통신판매중개자로서 판매 당사자가 아니며, 상품정보 및 거래 등에 대한 책임을 지지 않습니다.</p>
+          <p>COPYRIGHT © DAMGOM ALL RIGHTS RESERVED.</p>
+        </Col>
+      </Row>
 
-        <p>호스팅서비스:㈜농담곰</p>
-        <p>사이트 내 판매 상품 중 일부는 ㈜농담곰에 입점한 개별판매자가 판매하는 상품이 포함되어 있습니다. 해당 상품의 경우
-          ㈜농담곰은 통신판매중개자로서 판매 당사자가 아니며, 상품정보 및 거래 등에 대한 책임을 지지 않습니다.</p>
-        <p>COPYRIGHT © DAMGOM ALL RIGHTS RESERVED.</p>
 
 
     </footer>
--- src/components/search/FilterPrice.js
+++ src/components/search/FilterPrice.js
@@ -1,44 +1,65 @@
 import { useContext, useState } from "react";
 import { DataContext } from "../../MainData";
+import { ButtonGroup, ToggleButton, ToggleButtonGroup } from "react-bootstrap";
 
 export default function FilterPrice(props) {
     const { data, setFilter } = useContext(DataContext);
 
+    const [selectedButton, setSelectedButton] = useState(null);
+
+    const handleButtonClick = (value) => {
+        // 클릭된 버튼이 현재 선택된 값이 아니라면 선택한 값을 변경합니다.
+        if (selectedButton !== value) {
+            setSelectedButton(value);
+
+        } else {
+            // 이미 선택된 버튼을 다시 클릭했을 때는 선택을 해제합니다.
+            setSelectedButton(null);
+
+        }
+        // 여기에 필요한 로직을 추가할 수 있습니다. (선택된 값에 따른 처리 등)
+    };
+
     return (
         <li>
-            <label>
-                <input
-                    type="radio"
-                    name="price"
-                    onChange={(e) => {
-                        if (props.categoryCheck === "") {
-                            props.setPriceCheck(props.title);
-                            const dataCopy = [...data];
-                            const update = dataCopy.filter((item) => {
-                                return (
-                                    parseInt(props.startPrice) <= parseInt(item.lprice) &&
-                                    parseInt(item.lprice) < parseInt(props.endPrice)
-                                );
-                            });
-                            setFilter(update);
-                        } else {
-                            props.setPriceCheck(props.title);
-                            const dataCopy = [...data];
-                            const update = dataCopy.filter((item) => {
-                                return (
-                                    parseInt(props.startPrice) <= parseInt(item.lprice) &&
-                                    parseInt(item.lprice) < parseInt(props.endPrice)
-                                );
-                            });
-                            const update2 = update.filter((item) => {
-                                return item.category3 == props.categoryCheck;
-                            });
-                            setFilter(update2);
-                        }
-                    }}
-                />
-                {props.title}원
-            </label>
+            <ButtonGroup>
+                <ToggleButtonGroup type="radio" name="price">
+                    <ToggleButton
+                        variant={selectedButton === props.title ? "success" : "outline-success"}
+                        onClick={() => {
+                            handleButtonClick(props.title);
+                            if (props.categoryCheck === "") {
+                                props.setPriceCheck(props.title);
+                                const dataCopy = [...data];
+                                const update = dataCopy.filter((item) => {
+                                    return (
+                                        parseInt(props.startPrice) <= parseInt(item.lprice) &&
+                                        parseInt(item.lprice) < parseInt(props.endPrice)
+                                    );
+                                });
+                                setFilter(update);
+                            } else {
+                                props.setPriceCheck(props.title);
+                                const dataCopy = [...data];
+                                const update = dataCopy.filter((item) => {
+                                    return (
+                                        parseInt(props.startPrice) <= parseInt(item.lprice) &&
+                                        parseInt(item.lprice) < parseInt(props.endPrice)
+                                    );
+                                });
+                                const update2 = update.filter((item) => {
+                                    return item.category3 == props.categoryCheck;
+                                });
+                                setFilter(update2);
+                            }
+                        }}
+                    >
+                        {props.title}원
+                    </ToggleButton>
+                </ToggleButtonGroup>
+
+
+            </ButtonGroup>
         </li>
     );
 }
