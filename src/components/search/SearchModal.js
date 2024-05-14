84b04c8d7ab68c7ca88c05b896a65b28e108518c
SUH(green) <suh240505@gmail.com>
2024-05-14 09:32:19 +0900

    SearchModal 주석 추가

--- src/components/search/SearchModal.js
+++ src/components/search/SearchModal.js
@@ -8,11 +8,13 @@ import './SearchModal.css';
 export default function SearchModal(props) {
 
 
-
+    // input value state
     const [input, setInput] = useState('')
+    // api 건드리는 state
     const { setSearch } = useContext(DataContext)
+    // 로컬스토리지 저장하는 state
     const [searchHistory, setSearchHistory] = useState('')
-
+    // 로컬스토리지 읽고(key : search) json으로 변경해서 search 변수에 담음
     const searchHistoryLC = localStorage.getItem("search")
     const search = JSON.parse(searchHistoryLC);
 
@@ -24,6 +26,7 @@ export default function SearchModal(props) {
         }
     }, []); // 한 번만 실행
 
+    // 모달창 생성(열고 닫는 조건은 GlobalNav에 있음)
     return (
         <div className="searchModal" style={{ zIndex: "1050" }} >
             <Form className="d-flex" style={{ width: "80%", maxWidth: "800px", margin: "auto", marginTop: "100px" }}>
@@ -47,34 +50,26 @@ export default function SearchModal(props) {
                     }}
                 />
 
-                {/* 검색어가 있으면 링크로 이동 : 아닐 시 이동 x */}
+                {/* 검색어가 있으면 링크로 이동 : 아닐 시 경고 및 이동x */}
                 {
                     input ?
                         (
                             <Link to='list'>
                                 <Button variant="outline-success" onClick={() => {
-                                    if (input == '') {
-                                        alert("검색어를 입력해주세요.")
-                                    }
-                                    else {
-                                        setSearch(input)
-                                        props.setModal(!props.modal)
+                                    // api에 input에 적은 단어를 search에 삽입
+                                    setSearch(input)
+                                    // 모달 닫음
+                                    props.setModal(!props.modal)
 
-                                        // 새로운 검색어를 searchHissetSearchHistory에 추가한 후 로컬 스토리지에 저장
-                                        const updatedSearchHistory = [...searchHistory, input];
-                                        setSearchHistory(updatedSearchHistory);
-                                        localStorage.setItem("search", JSON.stringify(updatedSearchHistory));
-                                    }
+                                    // 새로운 검색어를 searchHissetSearchHistory에 추가한 후 로컬 스토리지에 저장
+                                    const updatedSearchHistory = [...searchHistory, input];
+                                    setSearchHistory(updatedSearchHistory);
+                                    localStorage.setItem("search", JSON.stringify(updatedSearchHistory));
                                 }}>Search</Button>
                             </Link>
                         ) : (
                             <Button variant="outline-success" onClick={() => {
-                                if (input == '') {
-                                    alert("검색어를 입력해주세요.")
-                                }
-                                else {
-                                    setSearch(input)
-                                }
+                                alert("검색어를 입력해주세요.")
                             }}>
                                 Search</Button>
                         )
@@ -88,7 +83,14 @@ export default function SearchModal(props) {
                         search.map((item, index) => {
                             return (
                                 <>
-                                    <Button variant="dark" key={index} onClick={() => { setSearch(item); props.setModal(!props.modal) }} style={{ margin: "20px" }}>{item}</Button>
+                                    <Button
+                                        variant="dark"
+                                        key={index}
+                                        onClick={() => {
+                                            setSearch(item);
+                                            props.setModal(!props.modal)
+                                        }}
+                                        style={{ margin: "20px" }}>{item}</Button>
                                 </>
                             )
                         })
