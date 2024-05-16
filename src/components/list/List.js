7cd45fdbdfb645d20ee7c34acee013c3aadfbc32
chomin(green) <doaiso381@naver.com>
2024-05-16 12:02:40 +0900

    리스트 수정

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -21,14 +21,14 @@ export default function List() {
 
     const startIndex = (page - 1) * itemsPerPage;    // 시작 인덱스
     const endIndex = Math.min(startIndex + itemsPerPage, filter.length);      // 마지막 인덱스
-    const currentItems = filter.slice(startIndex, endIndex);      // 한 페이지당 보여줄 아이템 데이터에서 값 가져오기
+    // const currentItems = filter.slice(startIndex, endIndex);      // 한 페이지당 보여줄 아이템 데이터에서 값 가져오기
     const totalPages = Math.ceil(filter.length / itemsPerPage);      // 전체 페이지 수 계산
 
 
     return (
         <>
             <Row xs={1} md={3} className="g-4">
-                {currentItems.map((item, index) => (
+                {filter.slice(startIndex, endIndex).map((item, index) => (
                     <Col key={index} className="d-flex justify-content-center align-items-center">
                         <Card style={{ width: '18rem', height: '32rem' }}>
                             <Link to={'/detail'} onClick={() => { setDetail(item) }}>
@@ -51,9 +51,9 @@ export default function List() {
                 ))}
             </Row>
 
-            <Pagination style={{justifyContent: 'center', padding: '3%'}}>
+            <Pagination style={{ justifyContent: 'center', padding: '3%' }}>
                 {[...Array(totalPages)].map((a, index) => (
-                    <Pagination.Item key={index + 1} active={page === index+1} onClick={() => setPage(index + 1)}>{index + 1}</Pagination.Item>
+                    <Pagination.Item key={index + 1} active={page === index + 1} onClick={() => setPage(index + 1)}>{index + 1}</Pagination.Item>
                 ))}
             </Pagination>
 
